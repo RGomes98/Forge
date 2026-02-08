c417745f7dd5020349bd58766a901ff5e64e7ff0
Renan Gomes <renango2015@gmail.com>
2026-02-08 20:36:23 -0300

    refactor(macros): improve route declaration with

--- crates/forge/src/lib.rs
+++ crates/forge/src/lib.rs
@@ -1,7 +1,11 @@
+pub use forge_http;
+pub use forge_router;
+
 pub mod prelude {
     pub use forge_config::{Config, ConfigError};
     pub use forge_http::{Headers, HttpError, HttpStatus, Params, Request, Response};
-    pub use forge_macros::main;
-    pub use forge_router::{Router, delete, get, post, put, routes};
+    pub use forge_router::Router;
     pub use forge_server::{Listener, ListenerOptions};
 }
+
+pub use forge_macros::{delete, get, head, options, patch, post, put, route};
