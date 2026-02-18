e64daa54af84305e6f425777bf26c4fbc0d0a816
Renan Gomes <renango2015@gmail.com>
2026-02-17 21:12:40 -0300

    docs(example): update route examples, README.md and config.toml

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::{net::Ipv4Addr, sync::Arc, time::Duration};
+use std::{net::Ipv4Addr, sync::Arc};
 
 use forge::prelude::*;
-use monoio::time;
 
-pub struct State {
-    pub db: PgActor,
-    pub version: &'static str,
+struct State {
+    db: Database,
+    version: &'static str,
 }
 
-#[monoio::main]
-async fn main() {
+fn main() {
     let mut router: Router<State> = Router::new();
 
     let listener_options: ListenerOptions = ListenerOptions {
@@ -18,73 +16,96 @@ async fn main() {
         host: Config::from_env("HOST").unwrap_or_else(|_| Ipv4Addr::new(127, 0, 0, 1)),
     };
 
-    let database_options: PgOptions = PgOptions {
-        pool_size: Config::from_env("DB_POOL_SIZE").unwrap_or(8),
-        database_url: Config::from_env("DB_URL").unwrap_or_default(),
+    let database_options: DatabaseOptions = DatabaseOptions {
+        url: Config::from_env("DB_URL").unwrap_or_default(),
+        threads: Config::from_env("DB_THREADS").unwrap_or(8),
         inflight_per_conn: Config::from_env("DB_INFLIGHT_PER_CONN").unwrap_or(32),
     };
 
     let state: State = State {
         version: "Forge Example v0.1.0",
-        db: PgActor::new(database_options).expect("failed to initialize database"),
+        db: Database::new(database_options).expect("failed to initialize database"),
     };
 
-    router.register(api_version_handler);
-    router.register(db_version_handler);
-    router.register(ping_handler);
-    router.register(health_handler);
-    router.register(user_handler);
-    router.register(store_handler);
+    router.register(ping);
+    router.register(version);
+    router.register(get_users);
+    router.register(create_user);
+    router.register(reset_database);
+    router.register(populate_database);
 
-    if let Err(e) = Listener::new(router, listener_options).with_state(state).run() {
-        eprintln!("failed to initialize server {e}")
-    }
+    Listener::new(router, listener_options)
+        .with_state(state)
+        .run()
+        .expect("failed to initialize server")
 }
 
-#[forge::get("/api/version")]
-pub async fn api_version_handler(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
+#[forge::get("/ping")]
+async fn ping(_req: Request<'_>) -> Response<'static> {
+    Response::new(HttpStatus::Ok).text("OK")
+}
+
+#[forge::get("/version")]
+async fn version(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
     Response::new(HttpStatus::Ok).text(state.version)
 }
 
-#[forge::get("/db/version")]
-pub async fn db_version_handler(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
-    match state.db.query("SELECT version()", vec![]).await {
-        Ok(version) => Response::new(HttpStatus::Ok).json(version.as_objects()),
+#[forge::get("/users")]
+async fn get_users(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
+    match state.db.query("SELECT * FROM users", vec![]).await {
+        Ok(users) => Response::new(HttpStatus::Ok).json(users.as_objects()),
         Err(e) => HttpError::new(HttpStatus::InternalServerError, e.to_string()).into(),
     }
 }
 
-#[forge::get("/ping")]
-async fn ping_handler(req: Request<'_>) -> Response<'static> {
-    let headers: Headers = req.headers;
-    println!("Headers: {headers:#?}");
-    Response::new(HttpStatus::Ok).text("pong!")
-}
-
-#[forge::get("/health")]
-async fn health_handler(_req: Request<'_>) -> Response<'static> {
-    Response::new(HttpStatus::Ok).text("OK")
-}
-
-#[forge::get("/user")]
-async fn user_handler(_req: Request<'_>) -> Response<'static> {
-    time::sleep(Duration::from_secs(5)).await;
-    Response::new(HttpStatus::Ok).body(r#"{"name":"John Doe","age":18}"#)
-}
-
-#[forge::get("/store/:store_id/customer/:customer_id")]
-async fn store_handler(req: Request<'_>) -> Response<'static> {
-    let Some(id_str) = req.params.get("store_id") else {
-        return HttpError::new(HttpStatus::BadRequest, "missing parameter \"store_id\"").into();
+#[forge::post("/user/:username")]
+async fn create_user(req: Request<'_>, state: Arc<State>) -> Response<'static> {
+    let Some(username) = req.params.get("username") else {
+        return HttpError::new(HttpStatus::BadRequest, "missing parameter \"username\"").into();
     };
 
-    let Ok(store_id) = id_str.parse::<i32>() else {
-        return HttpError::new(HttpStatus::BadRequest, "parameter \"store_id\" must be a valid integer").into();
-    };
+    let sql: &str = "INSERT INTO users (username) VALUES ($1) RETURNING *";
+    let args: Vec<SqlArg> = vec![SqlArg::Text((*username).into())];
 
-    if store_id < 1 {
-        return HttpError::new(HttpStatus::BadRequest, "parameter \"store_id\" must be greater than 0").into();
+    match state.db.query(sql, args).await {
+        Ok(user) => Response::new(HttpStatus::Created).json(user.as_objects()),
+        Err(e) => HttpError::new(HttpStatus::InternalServerError, e.to_string()).into(),
+    }
+}
+
+#[forge::post("/reset")]
+async fn reset_database(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
+    if let Err(e) = state.db.query("DROP TABLE IF EXISTS users", vec![]).await {
+        return HttpError::new(HttpStatus::InternalServerError, e.to_string()).into();
     }
 
-    Response::new(HttpStatus::Ok).json(req.params)
+    let sql: &str = r#"
+    CREATE TABLE users (
+        id BIGSERIAL PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
+        active BOOLEAN DEFAULT true
+    )
+    "#;
+
+    match state.db.query(sql, vec![]).await {
+        Ok(..) => Response::new(HttpStatus::Ok).text("table \"users\" reseted successfully!"),
+        Err(e) => HttpError::new(HttpStatus::InternalServerError, e.to_string()).into(),
+    }
+}
+
+#[forge::post("/populate")]
+async fn populate_database(_req: Request<'_>, state: Arc<State>) -> Response<'static> {
+    let sql: &str = "INSERT INTO users (username, active) VALUES ($1, $2), ($3, $4)";
+
+    let args: Vec<SqlArg> = vec![
+        SqlArg::Text("john_doe".into()),
+        SqlArg::Bool(false),
+        SqlArg::Text("jane_doe".into()),
+        SqlArg::Bool(false),
+    ];
+
+    match state.db.query(sql, args).await {
+        Ok(..) => Response::new(HttpStatus::Created).text("database successfully seeded!"),
+        Err(e) => HttpError::new(HttpStatus::InternalServerError, e.to_string()).into(),
+    }
 }
